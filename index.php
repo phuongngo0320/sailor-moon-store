0326ab506bdb07de70cade3cf5defb4b7fce3483
Phuong <phuongngovan2003@gmail.com>
2023-12-01 10:15:22 +0700

    Implement homepage UI

--- index.php
+++ index.php
@@ -4,10 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sailor Moon Store Online</title>
-    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Bootstrap -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- CSS & JS -->
+    <link rel="stylesheet" href="css/index.css">
     <script defer src="js/index.js"></script>
 </head>
 <body>
-    <h1>Welcome</h1>
+    <header>
+        <div class="upper">
+            <div class="logo">
+                <a href="#">
+                    <img src="https://sailormoon-store.com/img/usr/common/logo.png" alt="Logo">
+                </a>
+            </div>
+
+            <div class="search-bar">
+                <input type="search" name="search" id="search">
+
+                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+            </div>
+            
+            <div class="social-media">
+                <span><a href="#"><i class="fa-brands fa-x-twitter"></i></a></span>
+                <span><a href="#"><i class="fa-brands fa-instagram"></i></a></span>
+            </div>
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="product.php">Shop</a></li>
+                <li><a href="bill.php">Bills</a></li>
+                <li><a href="employee.php">Employees</a></li>
+                <li><a href="stat.php">Statistics</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Welcome</h1>
+        <h2>月に代わってお仕置きよ!</h2>
+    </main>
+    <footer>
+        <p>2023 No Copyright by Sailor Moon Team HCMUT</p>
+    </footer>
 </body>
 </html>
