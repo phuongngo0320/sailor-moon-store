a87ba90e2392d378252001f56a4d52a83301fd7d
Phuong <phuongngovan2003@gmail.com>
2023-12-01 11:35:05 +0700

    Add simple Statistics UI

---
+++ stat.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Statistics</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- CSS & JS -->
+    <link rel="stylesheet" href="css/stat.css">
+    <script defer src="js/stat.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="upper">
+            <div class="logo">
+                <a href="#">
+                    <img src="https://sailormoon-store.com/img/usr/common/logo.png" alt="Logo">
+                </a>
+            </div>
+
+            <div class="search-bar">
+                <input type="search" name="search" id="search">
+
+                <span><i class="fa fa-search" aria-hidden="true"></i></span>
+            </div>
+
+            <div class="social-media">
+                <span><a href="#"><i class="fa-brands fa-x-twitter"></i></a></span>
+                <span><a href="#"><i class="fa-brands fa-instagram"></i></a></span>
+            </div>
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="product.php">Shop</a></li>
+                <li><a href="bill.php">Bills</a></li>
+                <li><a href="employee.php">Employees</a></li>
+                <li><a href="#">Statistics</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Statistics</h1>
+
+
+
+
+        <div class="row stat-list">
+
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Chi nhánh Hà Nội</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Total Revenue</h6>
+
+                                        <p class="figure">1000</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Average Rating</h6>
+                                        <p class="figure">4.7</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Chi nhánh Hà Nội</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Total Revenue</h6>
+
+                                        <p class="figure">1000</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Average Rating</h6>
+                                        <p class="figure">4.7</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Chi nhánh Hà Nội</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Total Revenue</h6>
+
+                                        <p class="figure">1000</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Average Rating</h6>
+                                        <p class="figure">4.7</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Chi nhánh Hà Nội</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Total Revenue</h6>
+
+                                        <p class="figure">1000</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Average Rating</h6>
+                                        <p class="figure">4.7</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </main>
+    <footer>
+        <p>2023 No Copyright by Sailor Moon Team HCMUT</p>
+    </footer>
+</body>
+
+</html>
